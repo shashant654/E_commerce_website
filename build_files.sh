81745383afbc066cac2895a00a64754093ecaa6a
Shashant Shekhar <shashantshekhar10@gmail.com>
2024-06-13 04:17:38 +0530

    third commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,29 @@
+# echo "BUILD START"
+# python3.9 -m pip install -r requirements.txt
+# python3.9 manage.py collectstatic --noinput --clear
+# echo "BUILD END "
+
 echo "BUILD START"
+
+# Ensure pip is installed
+python3.9 -m ensurepip
+if [ $? -ne 0 ]; then
+    echo "Failed to install pip"
+    exit 1
+fi
+
+# Install requirements
 python3.9 -m pip install -r requirements.txt
+if [ $? -ne 0 ]; then
+    echo "Failed to install requirements"
+    exit 1
+fi
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END "
+if [ $? -ne 0 ]; then
+    echo "Failed to collect static files"
+    exit 1
+fi
+
+echo "BUILD END"
